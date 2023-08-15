ad682498331621fd62bf83dfa3f8251af829869c
AOZHU CHEN <jewelchen229@gmail.com>
2023-08-15 13:40:49 +0200

    Update download_50k.sh

--- download_50k.sh
+++ download_50k.sh
@@ -3,6 +3,16 @@
 # Set the name of the file to store download failures
 failure_file="DownloadFailure.txt"
 
+# Check if the destination path is provided as an argument
+if [ -z "$1" ]; then
+    echo "Usage: $0 <destination_path>"
+    exit 1
+fi
+
+# Store the destination path from the command-line argument
+destination_path="$1"
+
+
 # Read each line from the txt file, with format "video_id download_link"
 while IFS=' ' read -r video_id download_link; do
     echo "Downloading video ${video_id}..."
@@ -17,10 +27,10 @@ while IFS=' ' read -r video_id download_link; do
     else
         echo "Video ${video_id} downloaded successfully"
     fi
-done < your_input.txt
+done < ChinaOpen-50K-url.txt
 
 # Count download information
-total_links=$(wc -l < your_input.txt)
+total_links=$(wc -l < ChinaOpen-50K-url.txt)
 successful_downloads=$(ls -1 *.mp4 | wc -l)
 failed_downloads=$(wc -l < "${failure_file}")
 
